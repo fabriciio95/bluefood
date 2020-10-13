c1955a0ffd1ed1acffc25652e05c0cd57681cfec
fabriciio95 <fabriciusiqueira@gmail.com>
2020-10-13 19:36:27 -0300

    Definindo critérios de validação com a Bean Validation API

--- src/main/java/br/com/bluefood/domain/cliente/Cliente.java
+++ src/main/java/br/com/bluefood/domain/cliente/Cliente.java
@@ -1,6 +1,9 @@
 package br.com.bluefood.domain.cliente;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import br.com.bluefood.domain.usuario.Usuario;
 import lombok.EqualsAndHashCode;
@@ -13,8 +16,14 @@ import lombok.Setter;
 @Entity
 @SuppressWarnings("serial")
 public class Cliente extends Usuario {
-
+	
+	@NotBlank(message = " O CPF não pode ser vazio")
+	@Pattern(regexp = "[0-9]{11}", message = "O CPF possui formato inválido!")
+	@Column(length = 11, unique = true, nullable = false)
 	private String cpf;
 	
+	@NotBlank(message = " O CEP não pode ser vazio")
+	@Pattern(regexp = "[0-9]{8}", message = "O CEP possui formato inválido!")
+	@Column(length = 8, nullable = false)
 	private String cep;
 }
