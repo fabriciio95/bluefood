a7595c5e230cd8c1f5cce323f510acf71b8590bc
fabriciio95 <fabriciusiqueira@gmail.com>
2020-10-16 13:29:07 -0300

    Configurando a segurança da aplicação - Parte 1

--- src/main/java/br/com/bluefood/util/StringUtils.java
+++ src/main/java/br/com/bluefood/util/StringUtils.java
@@ -1,5 +1,8 @@
 package br.com.bluefood.util;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class StringUtils {
 
 	public static boolean isEmpty(String str) {
@@ -15,8 +18,7 @@ public class StringUtils {
 			return null;
 		}
 		
-		//PasswordEncoder enconder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		//return enconder.encode(rawString);
-		return rawString;
+		PasswordEncoder enconder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return enconder.encode(rawString);
 	}
 }
