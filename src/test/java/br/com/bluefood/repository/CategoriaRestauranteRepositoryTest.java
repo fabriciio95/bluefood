5309669c298903053cdc0908a3f87554e19cb6be
fabriciio95 <fabriciusiqueira@gmail.com>
2020-10-30 20:34:36 -0300

    Criando testes para repositórios - 1

---
+++ src/test/java/br/com/bluefood/repository/CategoriaRestauranteRepositoryTest.java
@@ -0,0 +1,40 @@
+package br.com.bluefood.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import br.com.bluefood.domain.restaurante.CategoriaRestaurante;
+import br.com.bluefood.domain.restaurante.CategoriaRestauranteRepository;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class CategoriaRestauranteRepositoryTest {
+	
+	@Autowired
+	private CategoriaRestauranteRepository categoriaRestauranteRepository;
+
+	@Test
+	public void testInsertAndDelete() throws Exception{
+		assertThat(categoriaRestauranteRepository).isNotNull();
+		
+		CategoriaRestaurante cr = new CategoriaRestaurante();
+		cr.setNome("Chinesa");
+		cr.setImage("chinesa.png");
+		categoriaRestauranteRepository.saveAndFlush(cr);
+		
+		assertThat(cr.getId()).isNotNull();
+		
+		CategoriaRestaurante cr2 = categoriaRestauranteRepository.findById(cr.getId()).orElseThrow();
+		assertThat(cr.getNome()).isEqualTo(cr2.getNome());
+		
+		assertThat(categoriaRestauranteRepository.findAll()).hasSize(7);
+		
+		categoriaRestauranteRepository.delete(cr);
+		
+		assertThat(categoriaRestauranteRepository.findAll()).hasSize(6);
+	}
+}
